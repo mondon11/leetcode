bd6fb86545c0ce6890db609e665d45b0e80fd749
mondon11 <houjiateng@aliyun.com>
2019-11-08 22:28:37 +0800

    20191108

--- 0004findMedianSortedArrays.py
+++ 0004findMedianSortedArrays.py
@@ -30,6 +30,47 @@ class Solution(object):
         else:
             return 0.5*(li[len(li)//2-1]+li[len(li)//2])
 
+    def findMedianSortedArrays1(self, nums1, nums2):
+        m = len(nums1)
+        n = len(nums2)
+        if m > n:
+            nums1_tmp = nums1
+            nums2_tmp = nums2
+            nums1 = nums2_tmp
+            nums2 = nums1_tmp
+            m = len(nums1)
+            n = len(nums2)
+        iMin,iMax = 0,m
+        while(iMin<=iMax):
+            i = (iMax+iMin)//2
+            j = (m+n+1)//2-i
+            if(j != 0 and i != m and nums2[j-1] > nums1[i]):
+                iMin = i+1
+            elif(i !=0 and j != n and nums1[i-1] > nums2[j]):
+                iMax = i-1
+            else:
+                if i == 0:
+                    maxLeft = nums2[j-1]
+                elif j == 0:
+                    maxLeft = nums1[i-1]
+                else:
+                    maxLeft = max(nums1[i-1],nums2[j-1])
+                if (m+n)%2 != 0:
+                    return maxLeft
+
+                if i == m:
+                    minRight = nums2[j]
+                elif j == n:
+                    minRight = nums1[i]
+                else:
+                    minRight = min(nums1[i],nums2[j])
+
+
+                return (maxLeft+minRight)/2.0
+
+
+
+
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.findMedianSortedArrays([1,2],[3,4]))
+    print(sol.findMedianSortedArrays1([1,2],[3,4]))
